4bd1ed0f1a0910828402ef39d2d9cbf4109739fc
LLdaniel <daniel.adlkofer@freenet.de>
2020-02-09 20:12:51 +0100

    added Belegung for Weichen --> input endless loop

--- worker.cpp
+++ worker.cpp
@@ -1,41 +1,43 @@
 #include "worker.h"
 #include <iostream>
 #include <wiringPi.h>
-worker::worker( std::vector<Block*> allBlocks, std::vector<int> allpins ){
+worker::worker( std::vector<std::pair<int,Block*>> allBlocks, std::vector<std::pair<int,Weiche*>> allWeichen ){
   blocklist = allBlocks;
-  pins = allpins;
+  weichenlist = allWeichen;
 }
 
 worker::~worker(){
 
 }
 
-void worker::addBlockList( Block* moreBlocks ){
-  blocklist.push_back( moreBlocks );
+void worker::addBlockList( int morePins, Block* moreBlocks ){
+  std::pair<int,Block*> hilfspair = std::make_pair(morePins,moreBlocks);
+  blocklist.push_back( hilfspair );
 }
 
 void worker::showBlocks(){
   std::cout<<"************************************************************"<<std::endl;
   std::cout<<"*** Dies sind die Blöcke, die ständig überwacht werden   ***"<<std::endl;
   for( unsigned int i = 0 ; i < blocklist.size() ; i++ ){//Durchläuft alle update Blocks
-    std::cout<<"***   "<<blocklist.at(i)->getName()<<"                                               ***"<<std::endl;
+    std::cout<<"***   "<<blocklist.at(i).first<<" -> "<<blocklist.at(i).second->getName()<<"                                           ***"<<std::endl;
   }
   std::cout<<"************************************************************"<<std::endl;
   std::cout<<""<<std::endl;
 }
 
-void worker::addPinList( int morePins ){
-  pins.push_back( morePins );
-}
-
-void worker::showPins(){
+void worker::showWeichen(){
   std::cout<<"************************************************************"<<std::endl;
-  std::cout<<"*** Dies sind alle Input-Pins                            ***"<<std::endl;
-  for( unsigned int i = 0 ; i < pins.size() ; i++ ){//Durchläuft alle pins
-    std::cout<<"***   "<<pins.at(i)<<"                                               ***"<<std::endl;
+  std::cout<<"*** Dies sind die Blöcke, die ständig überwacht werden   ***"<<std::endl;
+  for( unsigned int i = 0 ; i < blocklist.size() ; i++ ){//Durchläuft alle update Blocks
+    std::cout<<"***   "<<weichenlist.at(i).first<<" -> "<<weichenlist.at(i).second->getW_id()<<"                                           ***"<<std::endl;
   }
   std::cout<<"************************************************************"<<std::endl;
   std::cout<<""<<std::endl;
+}
+
+void worker::addWeichenList( int morePins, Weiche* moreWeichen ){
+  std::pair<int,Weiche*> hilfspair = std::make_pair(morePins,moreWeichen);
+  weichenlist.push_back( hilfspair );
 }
 
 void worker::quit(){
@@ -44,20 +46,45 @@ void worker::quit(){
 
 void worker::updateBelegt(){
   unsigned int i = 0;
-  int aktuell = -1;
-  while( update and pins.size() > 0){
-    //genaue Zuordung muss noch geklärt werden!! hier wäre es aktuell so, dass die pin und blockreihenfolge die zugehörige map ist... at(i) pin --> at(i) block
-    aktuell = digitalRead(pins.at(i));
-    if( aktuell == 0 ){ //read pins false = belegt || true = frei dagegen ist 0 = LOW , 1 = HIGH
-      blocklist.at(i)->setB_status(true);
+  int aktuellBlock = -1;
+  int aktuellWeiche = -1;
+  unsigned int maxindex = 0;
+  // welcher vector ist größer?
+  if( blocklist.size() >= weichenlist.size() ){
+    maxindex = blocklist.size();
+  }
+  if( weichenlist.size() > blocklist.size() ){
+    maxindex = weichenlist.size();
+  }
+  
+  // check, if there are pins für weichen oder blocks at all  
+  while( update and maxindex > 0){
+    //Zuordung direkt 1 zu 1 über das pair, schleif läuft parallel und stoppt je nach size etwas früher, bis es zum reset kommt (leerlauf für diesen vector)
+    if( i < blocklist.size() ){
+      aktuellBlock = digitalRead(blocklist.at(i).first);
+      if( aktuellBlock == 0 ){ //read pins false = belegt || true = frei dagegen ist 0 = LOW , 1 = HIGH
+	blocklist.at(i).second->setB_status(true);
+      }
+      if( aktuellBlock == 1 ){ //read pins
+	blocklist.at(i).second->setB_status(false);
+      }
     }
-    if( aktuell == 1 ){ //read pins
-      blocklist.at(i)->setB_status(false);
+    //////////////////////////
+    if( i < weichenlist.size() ){
+      aktuellWeiche = digitalRead(weichenlist.at(i).first);
+      if( aktuellWeiche == 0 ){ //read pins false = belegt || true = frei dagegen ist 0 = LOW , 1 = HIGH
+	weichenlist.at(i).second->setBelegung(true);
+      }
+      if( aktuellWeiche == 1 ){ //read pins
+	weichenlist.at(i).second->setBelegung(false);
+      }
     }
-    //gehe weiter in der Liste der Blöcke, und teste die, ansonsten von vorne -- vllt eher mit interrupt
-    if( i < pins.size() ){
+    //gehe weiter in der Liste der Blöcke, und teste die, ansonsten von vorne -- vllt eher mit interruptfunktion, nur wenn sich was ändert?
+    if( i < maxindex ){
       i++;
     }
     else i = 0; //ansonsten: reset
   }
+  //falls die schleife zu Ende kommt (entweder keine Einträge, oder quit Aufruf, dann beende den Worker)
+  emit finished();
 }
--- worker.h
+++ worker.h
@@ -1,23 +1,26 @@
 #ifndef WORKER_H
 #define WORKERR_H
 #include <Block.h>
+#include <Weiche.h>
 
 class worker : public QObject{// Vorbild nach https://mayaposch.wordpress.com/2011/11/01/how-to-really-truly-use-qthreads-the-full-explanation/
   Q_OBJECT
- public:
-  worker( std::vector<Block*> allBlocks, std::vector<int> allpins );
+public:
+  worker( std::vector<std::pair<int,Block*>> allBlocks, std::vector<std::pair<int,Weiche*>> allWeichen );
   ~worker();
-  void addBlockList( Block* moreBlocks );
+  void addBlockList( int morePins, Block* moreBlocks );
   void showBlocks();
-  void addPinList( int morePins );
-  void showPins();
-  public slots:
-    void updateBelegt(); // updates von allen Belegtstatus über eine schleife
-    void quit();         // wenn Programm beendet, dann terminate worker
- private:
-    bool update = true; //update gewünscht yes = true || no = false
-    std::vector<Block*> blocklist; //alle Blöcke, die nun upgedated werden
-    std::vector<int> pins; //alle input pins
-    
+  void addWeichenList( int morePins, Weiche* moreWeichen );
+  void showWeichen();
+public slots:
+  void updateBelegt(); // updates von allen Belegtstatus über eine schleife
+  void quit();         // wenn Programm beendet, dann terminate worker
+signals:
+  void finished();
+private:
+  bool update = true; //update gewünscht yes = true || no = false
+  std::vector< std::pair<int,Block*>> blocklist;                //alle Blöcke, die nun upgedated werden
+  std::vector< std::pair<int,Weiche*>> weichenlist;             //alle Weichen, die nun upgedated werden (Belegung)
+  
 };
 #endif // WORKER_H
